a83b5dacd8c7f42d619021faf0b34197b43d8d12
Rakeshkumar Desai <rakeshkumar.desai@mindtree.com>
2020-02-25 23:02:13 +0530

    Added first commit

---
+++ AkavacheDEmo/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Reactive.Linq;
+using Akavache;
+
+namespace AkavacheDEmo
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Akavache.Registrations.Start("AkavacheDEmo");
+            
+            
+        }
+        async void SetData(System.Object sender, System.EventArgs e)
+        {
+            var persondata = new Person { Name = "RD", Age = 10 };
+            var data = await BlobCache.UserAccount.InsertObject("person", persondata);
+        }
+
+        async void GetData(System.Object sender, System.EventArgs e)
+        {
+            var data = await BlobCache.UserAccount.GetObject<Person>("person");
+          
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
